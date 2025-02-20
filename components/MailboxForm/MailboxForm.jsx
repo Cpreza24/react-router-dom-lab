0d4aa531db20edc5569d4627fe47c591ee335f53
Cesar Preza <cpreza93@yahoo.com>
2025-02-19 20:45:46 -0800

    adds mailbox form data

--- components/MailboxForm/MailboxForm.jsx
+++ components/MailboxForm/MailboxForm.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 
 function MailboxForm() {
+  const [name, setName] = useState('');
+  const [boxSize, setBoxSize] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('submit button clicked');
   };
 
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <>
       <h1>Mailbox Form</h1>
-      <form action=''>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='box-owner'>Box Owner</label>
-        <input type='text' />
+        <input
+          type='text'
+          placeholder='enter your name'
+          onChange={handleChange}
+        />
         <label htmlFor='box-size'>Size</label>
-        <select name='box-size' id='box-size'>
-          <option value='sm'>Small</option>
-          <option value='md'>Medium</option>
-          <option value='lg'>Large</option>
+        <select
+          name='box-size'
+          id='box-size'
+          placeholder='Small'
+          onChange={handleChange}
+        >
+          <option value='small'>Small</option>
+          <option value='medium'>Medium</option>
+          <option value='large'>Large</option>
         </select>
-        <button type='submit' onClick={handleSubmit}>
-          Submit
-        </button>
+        <button type='submit'>Submit</button>
       </form>
     </>
   );
