ad777f316da63d67898150675a1531f7affc6af1
Cesar Preza <cpreza93@yahoo.com>
2025-02-19 20:27:32 -0800

    adds Routes to aoo.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import NavBar from '../components/NavBar/NavBar';
+import MailboxList from '../components/MailboxList/MailboxList';
+import MailboxForm from '../components/MailboxForm/MailboxForm';
+import MailboxDetails from '../components/MailboxDetails/MailboxDetails';
+import Home from '../components/Home/Home';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/mailboxes' element={<MailboxList />} />
+        <Route path='/new-mailbox' element={<MailboxForm />} />
+        <Route path='/mailboxes/:mailboxId' element={<MailboxDetails />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
